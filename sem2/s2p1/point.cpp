6cc2980406a1487cf92c764529068a31eaaf2082
Hanriel <hanriel59@gmail.com>
2023-05-16 22:07:33 +0500

    Report for s2p1

--- sem2/s2p1/point.cpp
+++ sem2/s2p1/point.cpp
@@ -3,27 +3,23 @@
 #include <cmath>
 using namespace std;
 
-void point::Init(double X, double Y)
-{
+void point::Init(double X, double Y) {
 	_x = X;
 	_y = Y;
 }
 
-void point::Read()
-{
+void point::Read() {
 	cout << "X: ";
 	cin >> _x;
 	cout << "Y: ";
 	cin >> _y;
 }
 
-void point::Show()
-{
+void point::Show() {
 	cout << "X: " << _x << endl;
 	cout << "Y: " << _y << endl;
 }
 
-double point::Distance(double x1, double y1)
-{
+double point::Distance(double x1, double y1) {
 	return sqrt(powf(x1 - _x, 2) + powf(y1 - _y, 2));
 }
